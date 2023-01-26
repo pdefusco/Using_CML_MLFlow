c84a12e50db21006ab88d962cf28952679337da7
pdefusco <pauldefusco@gmail.com>
2023-01-25 18:55:09 -0800

    backing up progress

--- code/push_api.py
+++ code/push_api.py
@@ -4,7 +4,7 @@ import pandas as pd
 logged_model = '/home/cdsw/.experiments/umyb-mq5h-2v3x-j445/5t4w-q921-u7rz-1jfz/artifacts/model'
 
 def predict(args):
-  
+
   # Load model as a PyFuncModel.
   data = args.get('input')
   loaded_model = mlflow.pyfunc.load_model(logged_model)
