34e68f3334821fa3f62d18657d7f7ede299db64d
Paul de Fusco <pauldefusco@cloudera.com>
2023-03-09 00:28:43 +0000

    restructuring

--- code/part2/01_Active_Experiment.py
+++ code/part2/01_Active_Experiment.py
@@ -1,6 +1,11 @@
 import mlflow
 
+mlflow.set_experiment("active-experiment")
+
 mlflow.start_run()
+
 run = mlflow.active_run()
+
 print("Active run_id: {}".format(run.info.run_id))
+
 mlflow.end_run()
