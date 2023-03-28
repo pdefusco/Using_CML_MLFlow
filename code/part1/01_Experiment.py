bbab26723e7ea6d7d3b490573c82cf75f7992ad8
Paul de Fusco <pauldefusco@cloudera.com>
2023-03-28 00:33:36 +0000

    changerd hyperparams in script 1

--- code/part1/01_Experiment.py
+++ code/part1/01_Experiment.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    alpha = 0.5
+    l1_ratio = 0.5
     
     with mlflow.start_run():
       
